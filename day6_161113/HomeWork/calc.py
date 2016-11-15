908807a6239eab6a3fd1248fb8bdbebc70b1874d
zewei <w2341078@163.com>
2016-11-15 23:47:47 +0800

    calc添加注释

--- day6_161113/HomeWork/calc.py
+++ day6_161113/HomeWork/calc.py
@@ -2,12 +2,12 @@ import re
 
 cal = "1 - 2 * ( (60-30 +(-40/5) * (9-2*5/3 + 7 /3*99/4*2998 +10 * 568/14 )) - (-4*3)/ (16-3*2) )"
 
-strip_space = re.sub(r' +', '', cal)
+strip_space = re.sub(r' +', '', cal)  # 去掉无用的空格
 print(strip_space)
-a = re.search(r'\([^()]+\)', strip_space).group()
+a = re.search(r'\([^()]+\)', strip_space).group()  #找出最里层的括号
 
-no_kuohao = re.search(r'[^()]+', a).group()
+no_kuohao = re.search(r'[^()]+', a).group()  #去掉括号
 
-li_a = re.sub(r'\/', ' ', no_kuohao).split(" ")
-result = eval("%s / %s" % (li_a[0], li_a[1]))
+li_a = re.sub(r'\/', ' ', no_kuohao).split(" ")  # 序列化
+result = eval("%s / %s" % (li_a[0], li_a[1]))  #计算
 print(result)
