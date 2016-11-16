8c2eeb6c7cd4b64c28ebc5323fabd63852f4cfec
zewei <w2341078@163.com>
2016-11-17 00:02:01 +0800

    calc乘除模块调试

--- day6_161113/HomeWork/calc.py
+++ day6_161113/HomeWork/calc.py
@@ -14,53 +14,95 @@ no_kuohao = re.search(r'[^()]+', a).group()  #去掉括号
 
 
 # print(result)
+def plus(li_a):
+    num_1 = int(li_a[0])
+    num_2 = int(li_a[1])
+    return num_1 + num_2
+
+
+def minus(li_a):
+    num_1 = int(li_a[0])
+    num_2 = int(li_a[1])
+    return num_1 - num_2
+
+
+def multiply(li_a):
+    num_1 = int(li_a[0])
+    num_2 = int(li_a[1])
+    return num_1 * num_2
+
+
+def divide(li_a):
+    num_1 = int(li_a[0])
+    num_2 = int(li_a[1])
+    return num_1 / num_2
+
+
 
 
 def Operation(no_kuohao):
-    opt = re.findall(r'[\+\*\/]', no_kuohao)
+    opt_dic = {
+        '+': plus,
+        '-': minus,
+        '*': multiply,
+        '/': divide
+    }
+    opt = re.findall(r'[\+\*\/]', no_kuohao)  # 找出运算符
+    print("opt", opt)
+    li_num = re.sub(r'[\+\*\/]', ' ', no_kuohao).split(" ")  # 替换掉运算符,把数字存进列表
+    print("li_num", li_num)
+    if len(li_num) == 1:
+        if len(opt) == 0:  # 此处判断减负数
+            opt = ['+']  # 减负数实际上就是做加法
+            li_num = li_num[0].replace('--', ' ').split(" ")  # 把后面的负数替换成正数
+            # print(li_num[0].replace('--',' ').split(" "))
+            # print(li_num)
 
-    li_a = re.sub(r'[\+\*\/]', ' ', no_kuohao).split(" ")
-    if len(li_a) != 1:
-        # opt = li_a[1]
-        # print(li_a)
-        if len(opt) == 0:
-            opt = '-'
-        # print(opt)
-        print(opt)
-        num_1 = li_a[0]
-        num_2 = li_a[1]
-        print("%s%s%s" % (num_1, opt[0], num_2))
-        result = eval("%s%s%s" % (num_1, opt[0], num_2))
+    print(opt_dic[opt[0]](li_num))
+    # result = eval(li_a[0])
+    # return str(result)
 
-        return str(result)
-    else:
-        result = eval(li_a[0])
-        return str(result)
 
+Operation('-1-2')
 
 strip_space = strip_space.replace(a, Operation(no_kuohao))
 print(strip_space)
-# print(re.search(r'\([^()]+\)', strip_space).group())
 b = re.search(r'\([^()]+\)', strip_space).group()
-
-
-# print(re.search(r'-?\d[\*\/]-?\d', b).group())
+print(b)
 
 
 def chengchu(cheng_chu):
+    cheng_chu = re.search(r'[^()]+', cheng_chu).group()
     while True:
-        small_expression = re.search(r'(-?\d+\.?\d+)[\*\/](-?\d+\.?\d+)', cheng_chu).group()
-        print(small_expression)
+        small_expression = re.search(r'(-?\d+(\.\d+)?)[\*\/](-?\d+(\.?\d+)?)', cheng_chu).group()
+        # print(small_expression)
         result = Operation(small_expression)
-        print(result)
+        # print(result)
         cheng_chu = cheng_chu.replace(small_expression, result)
-        print(cheng_chu)
-        if len(re.sub(r'[\*\/]', ' ', no_kuohao).split(' ')) == 1:
+        # print(cheng_chu)
+        # print(re.sub(r'[\*\/]', ' ', cheng_chu).split(' '))
+        if len(re.sub(r'[\*\/]', ' ', cheng_chu).split(' ')) == 1:
             break
-    return no_kuohao
+    return cheng_chu
 
 
-print(chengchu(b))
+def jiajian(jia_jian):
+    while True:
+        small_expression = re.search(r'(-?\d+(\.\d+)?)[\+\-](-?\d+(\.?\d+)?)', jia_jian).group()
+        # print(small_expression)
+        result = Operation(small_expression)
+        # print(result)
+        jia_jian = jia_jian.replace(small_expression, result)
+        # print(cheng_chu)
+        # print(re.sub(r'[\*\/]', ' ', cheng_chu).split(' '))
+        if len(re.sub(r'[\+\-]', ' ', jia_jian).split(' ')) == 1:
+            break
+    return jia_jian
+
+
+c = chengchu(b)
+print(c)
+print(jiajian(c))
 
 
 
