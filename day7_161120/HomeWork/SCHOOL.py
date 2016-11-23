102aa5668838feb6eea421b60067802b0786b6c9
Administrator <w2341078@163.com>
2016-11-24 00:16:51 +0800

    day6 作业 python 实现计算器

--- day7_161120/HomeWork/SCHOOL.py
+++ day7_161120/HomeWork/SCHOOL.py
@@ -1,6 +1,11 @@
 class School(object):
+    school_master = {}
     def __init__(self, address):
         self.address = address
+
+    def create_school(self,address):
+
+        return address
 
     def create_class(self, course, teacher):
         pass
@@ -11,11 +16,16 @@ class School(object):
     def create_course(self, period, price):
         pass
 
+class Course(School):
+    def __init__(self):
+        pass
 
 class Teacher(School):
     def __init__(self, address):
         super(School, self).__init__(address)
 
+    def create_class(self, course, teacher):
+        pass
 
 class Student(School):
     tuition = 0
