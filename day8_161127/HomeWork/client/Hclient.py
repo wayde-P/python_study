42ba7a840501c6f5d71d40ffde35768f59dedea4
zewei <w2341078@163.com>
2016-12-03 11:39:05 +0800

    day 8 作业完成.简单的程序.能实现上传下载文本文件.能执行命令.并返回结果.

--- day8_161127/HomeWork/client/Hclient.py
+++ day8_161127/HomeWork/client/Hclient.py
@@ -1,16 +1,19 @@
 import socket
 import os
 import sys
+import time
 
 client = socket.socket()
 client.connect(("127.0.0.1", 8001))
 sys.path.append(os.getcwd())
 
-print(sys.path, os.getcwd())
+# print(sys.path, os.getcwd())
 while True:
-    msg = str(input("请输入:").strip())
+    msg = str(input("请输入[q]:").strip())
     if len(msg) == 0: continue
-    if msg.startswith("put"):
+    elif msg == "q":
+        break
+    elif msg.startswith("put"):
         msg_data = msg.encode()
         client.send(msg_data)
         base_list = os.listdir(os.path.dirname(os.path.abspath(__file__)))
@@ -25,32 +28,62 @@ while True:
             #             "file_size": os.path.getsize(file)}
             # send_data = eval(file_msg)
             client.send(str(os.path.getsize(file)).encode())
+            time.sleep(1)
             print(file)
             with open(file, "rb") as read_file:
                 for line in read_file:
                     print("send data ......", line)
                     client.send(line)
-
-
         else:
-            print("%s此文件不存在" % msg_li[1])
+            print("%s 此文件不存在" % msg_li[1])
         continue
-
-    client.send(msg.encode())
-    print("send", msg)
-    lenth = client.recv(1024)
-    if len(lenth) > 0:
-        data_len = int(lenth.decode())  # 获取返回长度
+    elif msg.startswith("get"):
+        msg_data = msg.encode()
+        msg_li = msg.split(" ")
+        client.send(msg_data)
+        check = int(client.recv(1024).decode())
+        if check:
+            # data_size = client.recv(1024) #获取文件大小
+            base_list = os.listdir(os.path.dirname(os.path.abspath(__file__)))
+            print(base_list)
+            if msg_li[1] in base_list:
+                file_name = "%s/%s_new" % (os.path.dirname(os.path.abspath(__file__)), msg_li[1])
+            else:
+                file_name = "%s/%s" % (os.path.dirname(os.path.abspath(__file__)), msg_li[1])
+            print(file_name)
+            with open(file_name, "wb") as write_file:
+                receive = client.recv(1024)
+                print("receive size : ", receive)
+                receive_size = int(receive.decode())
+                source_size = 0
+                new_data = b''
+                print("开始接收数据,文件大小:", receive_size)
+                while source_size < receive_size:
+                    new_data = client.recv(1024)
+                    print("接收数据: ", new_data.decode())
+                    source_size += len(new_data)
+                    print(source_size, receive_size)
+                    write_file.write(new_data)
+            print("接收完成.....")
+        else:
+            print("文件不存在", msg_li[1])
+            continue
     else:
-        continue
-
-    received_size = 0
-    received_data = b''
-    while received_size < data_len:
-        data = client.recv(1024)  # 返回的数据
-        received_data += data
-        received_size += len(data)
-
-    print("接收到服务器的信息:\n", received_data.decode(), data_len)
+        client.send(msg.encode())
+        print("send", msg)
+        lenth = client.recv(1024)
+        if len(lenth) > 0:
+            data_len = int(lenth.decode())  # 获取返回长度
+        else:
+            continue
+        received_size = 0
+        received_data = b''
+        while received_size < data_len:
+            data = client.recv(1024)  # 返回的数据
+            received_data += data
+            received_size += len(data)
+        print("接收到服务器的信息:\n", received_data.decode())
+        # print("接收到服务器的信息:\n", received_data.decode(), data_len)
     # print("接收到服务器的信息:", data)
     # dataa = client.recv(1024)
+client.close()
--- day8_161127/HomeWork/server/Hserver.py
+++ day8_161127/HomeWork/server/Hserver.py
@@ -2,6 +2,7 @@
 import socket
 import subprocess
 import os
+import time
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(("0.0.0.0", 8001))
@@ -16,42 +17,58 @@ while True:
         try:
             data = conn.recv(1024)
             print("recv from cli:", data)
-            receive_data = data.decode()
+            receive_data = data.decode()  # 获取传入的文件信息
             base_list = os.listdir(os.path.dirname(os.path.abspath(__file__)))
-            print("base_list:", base_list)
+            # print("base_list:", base_list)
             if receive_data.startswith("put"):
                 msg = receive_data.split(" ")
                 if msg[1] in base_list:
-                    file_name = "%s_new" % msg[1]
+                    file_name = "%s/%s_new" % (os.path.dirname(os.path.abspath(__file__)), msg[1])
                 else:
-                    file_name = msg[1]
+                    file_name = "%s/%s" % (os.path.dirname(os.path.abspath(__file__)), msg[1])
                 print("file name is:", file_name)
-                with open(file_name, "w+") as write_file:
+                with open(file_name, "wb") as write_file:
                     receive_size = int(conn.recv(1024).decode())
                     source_size = 0
                     new_data = b''
                     print("开始接收数据,文件大小:", receive_size)
                     while source_size < receive_size:
+                        # source_size = os.path.getsize(file_name)
                         print("接收数据.....")
-                        new_data = conn.recv(1024).decode()
+                        new_data = conn.recv(1024)
                         print(new_data)
                         source_size += len(new_data)
                         print(source_size, receive_size)
                         write_file.write(new_data)
+                print("接收完成.....")
                 continue
-
-
-
-
-
-
-
-            res_obj = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            res = res_obj.stdout.read()
-            data_len = str(len(res)).encode()
-            conn.send(data_len)
-            print(data_len)
-            conn.send(res)
+            elif receive_data.startswith("get"):
+                msg = receive_data.split(" ")  # 获取传入的文件信息
+                base_list = os.listdir(os.path.dirname(os.path.abspath(__file__)))
+                if msg[1] in base_list:
+                    file = "%s/%s" % (os.path.dirname(os.path.abspath(__file__)), msg[1])
+                    conn.send(b'1')
+                    print("get file name is :", file)
+                    # file_msg = {"file_name": msg_li[1],
+                    #             "file_size": os.path.getsize(file)}
+                    # send_data = eval(file_msg)
+                    conn.send(str(os.path.getsize(file)).encode())
+                    time.sleep(1)
+                    print(file)
+                    with open(file, "rb") as read_file:
+                        for line in read_file:
+                            # print("send data: ", line.decode())
+                            conn.send(line)
+                else:
+                    conn.send(b'0')
+            else:
+                res_obj = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                res = res_obj.stdout.read()
+                data_len = str(len(res)).encode()
+                time.sleep(0.2)
+                conn.send(data_len)
+                print(data_len)
+                conn.send(res)
         except ConnectionResetError as e:
             break
         except BrokenPipeError as e:
