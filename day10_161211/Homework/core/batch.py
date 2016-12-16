6fe6cedfe745f63a8049fe77ed2e2d9478ef1ce8
wayde <w2341078@163.com>
2016-12-16 16:22:45 +0800

    day10homework还差服务列表没有分析.分析完成即可

--- day10_161211/Homework/core/batch.py
+++ day10_161211/Homework/core/batch.py
@@ -2,6 +2,8 @@ import optparse
 import configparser
 import os
 import sys
+import multiprocessing as MP
+import paramiko
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
@@ -20,10 +22,30 @@ def main():
         exit("请带上 -c \"你的命令\"")
     elif options.host is None and options.group is None:
         exit("请输入 -h 或 -g ")
-    # elif (options.host is not None) or (options.group is not None):
     else:
         server_list = fetch_server_list(options.host, options.group)
         print(server_list)
+
+
+def launch(host, port, user, password, command):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname=host, port=port, username=user, password=password)
+    stdin, stdout, stderr = ssh.exec_command(command)
+    result = stdout.read()
+    print(result.decode())
+    ssh.close()
+
+
+def run(server):
+    p_list = []
+    for i in server.keys():
+        process = MP.Process(target=launch, args=())
+        process.start()
+        p_list.append(process)
+
+    for t in p_list:
+        t.join()
 
 
 def fetch_server_list(hosts, groups):
@@ -32,23 +54,35 @@ def fetch_server_list(hosts, groups):
     config.read(settings.group_file)  # 读取group配置文件
     all_server = config.sections()
     # print(all_server)
-    server_list = []  # 用来存储服务器列表
+    server_list = {}  # 用来存储服务器列表
 
     if hosts is not None:  # 传入的host不为空
         host_list = hosts.split(",")  # 切割传入的host
         for host in host_list:
             if host in all_server:  # host存在配置文件里
-                server_list.append(host)  # host加入列表里
+                server_list[host] = {}  # host加入列表里
+                server_list[host]["user"] = config[host]["user"]
+                server_list[host]["password"] = config[host]["password"]
+                server_list[host]["port"] = config[host]["port"]
+            else:
+                print(host, "is not in host configure ")
     if groups is not None:
         group_list = groups.split(",")
         for group in group_list:
             if group in all_server:
                 for host in config[group]["ip"].split(","):
-                    server_list.append(host)
+                    if host in all_server:  # host存在配置文件里
+                        server_list[host] = {}  # host加入列表里
+                        server_list[host]["user"] = config[host]["user"]
+                        server_list[host]["password"] = config[host]["password"]
+                        server_list[host]["port"] = config[host]["port"]
+                    else:
+                        print(host, "is not in host configure ")
+                        # server_list.append(host)
             else:
                 print("%s not in group.cfg" % group)
 
-    return set(server_list)
+    return server_list
 
 
 if __name__ == "__main__":
--- day10_161211/Homework/core/test.py
+++ day10_161211/Homework/core/test.py
@@ -2,18 +2,52 @@ import optparse
 import configparser
 import os
 import sys
+import paramiko
+import multiprocessing as mp
+
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
 
 from conf import settings
 
-config = configparser.ConfigParser()
-config.read(settings.host_file)
-config.read(settings.group_file)
-all_server = config.sections()
-print(config['192.168.1.1']['user'])
-for i in config['webserver']:
-    print(i)
 
-print(all_server)
+# config = configparser.ConfigParser()
+# config.read(settings.host_file)
+# config.read(settings.group_file)
+# all_server = config.sections()
+# print(config['192.168.1.1']['user'])
+# for i in config['webserver']:
+#     print(i)
+#
+# print(all_server)
+
+
+def launch(host, port, user, password, command):
+    # ssh_link=paramiko.Transport((host,port))
+    # ssh_link.connect(username=user,password=password)
+    # ssh = paramiko.SSHClient()
+    # ssh._ssh_link = ssh_link
+    # stdin,stdout,stderr = ssh.exec_command(command)
+    # print(stdout.read())
+    # ssh_link.close()
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname=host, port=port, username=user, password=password)
+    stdin, stdout, stderr = ssh.exec_command(command)
+    result = stdout.read()
+    print(result.decode())
+    ssh.close()
+
+
+t_list = []
+# for i in range(5):
+p = mp.Process(target=launch, args=(("192.168.0.101", 22, "root", "dianrudianru", "date")))
+p.start()
+# t_list.append(p)
+#
+# for t in t_list:
+#     t.join()
+
+# launch("192.168.0.101",22,"root","dianrudianru","df")
