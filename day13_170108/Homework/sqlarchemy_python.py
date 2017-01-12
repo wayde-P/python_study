a86afa6ef98ce120e1a28e3a3481c09414bd40d8
zewei <w2341078@163.com>
2017-01-13 01:17:37 +0800

    13天作业 开始研究sqlalchemy

--- day13_170108/Homework/sqlarchemy_python.py
+++ day13_170108/Homework/sqlarchemy_python.py
@@ -4,7 +4,8 @@ from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy import create_engine
 
 Base = declarative_base()
-engine = create_engine("mysql+pymysql://root:dianru@192.168.0.4:3306/zzw_host?charset=utf8", max_overflow=5)
+engine = create_engine("mysql+pymysql://zewei:zewei@192.168.50.116:3306/s15_13?charset=utf8", max_overflow=5)
+# engine = create_engine("mysql+pymysql://root:dianru@192.168.0.4:3306/zzw_host?charset=utf8", max_overflow=5)
 Session = sessionmaker(bind=engine)
 session = Session()
 
